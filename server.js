f8c3f767aae63499cd3401b334e1dd78eec889e9
Chris Milson <chris.w.milson@gmail.com>
2019-09-23 18:05:21 +0900

    Add console log on request to server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('*', (req, res) => {
+  console.log('I got a request!')
   res.send('This is the Shlappas Raspberry Pi server.')
 })
 
