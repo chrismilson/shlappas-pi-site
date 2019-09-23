96ddd81d8bee09357a2d7c7cf5abcd96fc5a349c
Chris Milson <chris.w.milson@gmail.com>
2019-09-23 19:00:46 +0900

    Serve static client files

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+app.use(bodyParser)
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build/index.html'))
