ae77edca37186712b1550cf5b3096605f096eb84
Chris Milson <chris.w.milson@gmail.com>
2019-09-23 18:11:04 +0900

    Add request count for debugging

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
+var requestCount = 0
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('*', (req, res) => {
-  console.log('I got a request!')
+  console.log(`I got a request! #${requestCount}`)
   res.send('This is the Shlappas Raspberry Pi server.')
 })
 
